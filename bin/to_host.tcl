122277f5d98d09ecea0714c712f6319ed0026be4
Zhipeng Dong <zhipengdongneu@gmail.com>
2017-11-27 10:00:26 +0800

    Add arm launch

--- bin/to_host.tcl
+++ bin/to_host.tcl
@@ -3,19 +3,22 @@
 # Launch neurobot from workstation
 set timeout 20
 
-# Get master ip
-
-# Here argv 0 is the 1st param
 set MASTER_IP 192.168.1.107
+
+# Get master ip
+if "$argc == 1" {
+    # Here argv 0 is the 1st param
+    set MASTER_IP "192.168.1.[lindex $argv 0]"
+}  else  {
+    puts "Use default IP: $MASTER_IP"
+}
 
 # ssh to remote server
 spawn ssh $MASTER_IP
-
 set PASSWORD aicrobo
 # If we do not have the first input, continue
 expect {
-"*yes/no" { send "yes\r"; exp_continue }
-"*password:" { send "$PASSWORD\r" }
+    "*yes/no" { send "yes\r"; exp_continue }
+    "*password:" { send "$PASSWORD\r" }
 }
-
 interact
